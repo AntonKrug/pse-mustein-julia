1322123d8737b43892ee9eb1bd8bcefe1f7f34a0
Anton Krug <30338596+AntonKrug@users.noreply.github.com>
2019-03-18 21:02:46 +0000

    Explaining resolution and buffer relation

--- fractals/fractal_configuration.h
+++ fractals/fractal_configuration.h
@@ -1,29 +1,36 @@
 /*
- *		Date:   2018/12/01
- *      Author: anton.krug@microchip.com
+ *		Date: 2018/12/01
+ *      Author: akrug
  */
 
 #ifndef SRC_FRACTAL_CONFIGURATION_H_
 #define SRC_FRACTAL_CONFIGURATION_H_
 
-// 10 bits => 1024
-// 9  bits => 512
-// 8  bits => 256
-// 7  bits => 128
-// 6  bits => 64
-// 5  bits => 32
-// 4  bits => 16
-// 3  bits => 8
+// When you increase the resolution, double check if the platform is allocating
+// enough memory for it:
+// machine LoadPlatformDescriptionFromString "mustein: Video.MusteinGenericGPU @ sysbus 0x10100000 { frameBufferSize: 0x100000; registers64bitAligned: true }"
+// Increasing the frameBufferSize might be necessary. When it's not specified it
+// will use its default value which should be enough for 1024x1024 resolution
+#define WIDTH_BITS  4  // WIDTH  = 1 << WIDTH_BITS
+#define HEIGHT_BITS 4  // HEIGHT = 1 << HEIGHT_BITS
 
-#define WIDTH_BITS 7   // WIDTH  = 1 << WIDTH_BITS
-#define HEIGHT_BITS 7  // HEIGHT = 1 << HEIGHT_BITS
+// 10 bits means 1024 pixels
+// 9  bits means 512  pixels
+// 8  bits means 256  pixels
+// 7  bits means 128  pixels
+// 6  bits means 64   pixels
+// 5  bits means 32   pixels
+// 4  bits means 16   pixels
+// 3  bits means 8    pixels
+
 
 #ifndef ANIMATION_SPEED
-#define ANIMATION_SPEED 0.02f // How large steps are done between the frames
+#define ANIMATION_SPEED 0.0005f // How large steps are done between the frames
 #endif
 
 
-#define WIDTH ( 1 << (WIDTH_BITS))
+// Do not edit these lines below, to edit the resolution edit the defines above
+#define WIDTH  ( 1 << (WIDTH_BITS))
 #define HEIGHT ( 1 << (HEIGHT_BITS))
 
 #endif /* SRC_FRACTAL_CONFIGURATION_H_ */
